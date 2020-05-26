70f770f3895d4eaee32a0db442eaeeae6ab2bf06
Manjot Singh <github@manjotsingh.xyz>
2020-05-26 18:30:45 -0400

    Add files via upload

---
+++ hukamnama/hukam.js
@@ -0,0 +1,64 @@
+$.getJSON(
+    "https://api.gurbaninow.com/v2//hukamnama/today", function(data){
+          console.log(data)
+          
+         var date =  data.date.gregorian.day + " " + data.date.gregorian.month + " " + data.date.gregorian.date + " " + data.date.gregorian.year;
+        
+        $('.date').append(date)
+        
+        var ndate = data.date.nanakshahi.punjabi.day + " " + data.date.nanakshahi.punjabi.date + " " + data.date.nanakshahi.punjabi.month + " "+ data.date.nanakshahi.punjabi.year;
+          
+        $('.ndate').append(ndate)
+        
+        
+        
+        var line0g = data.hukamnama[0].line.gurmukhi.akhar;
+        
+        console.log(line0g)
+        
+        
+        
+        for (i in data.hukamnama) {
+            x = data.hukamnama[i];
+                for (i in x) 
+                    
+                    y=x[i]
+                   
+            $('.gurbani').append(y.gurmukhi.akhar);
+                
+            }
+        
+        
+        for (i in data.hukamnama) {
+            x = data.hukamnama[i];
+                for (i in x) 
+                    
+                    y=x[i]
+                   
+            $('.punjabi').append(y.translation.punjabi.default.akhar);
+                
+            }
+        
+        for (i in data.hukamnama) {
+            x = data.hukamnama[i];
+                for (i in x) 
+                    
+                    y=x[i]
+                   
+            $('.english').append(y.translation.english.default);
+                
+            }
+                
+            })
+            
+            
+            
+        
+            
+            
+    
+        
+           
+
+        
+
